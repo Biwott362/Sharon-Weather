608444129c1f00e4ae95f1f9d6feb78b4f2c0f37
Sharon <156696215+Biwott362@users.noreply.github.com>
2024-04-27 23:31:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function refreshWeather(response){
    let humidityElement = document.querySelector("#humidity");
    let windSpeedElement = document.querySelector("#wind-speed");
    let timeElement = document.querySelector("#time");
-   let date = new Date(response.data.timezone * 1000);
+   let date = new Date(response.data.dt* 1000);
    let weatherIcon = document.querySelector("#icon");
 
 
@@ -39,11 +39,14 @@ function refreshWeather(response){
     weatherIcon.src = "src/thunderstorms-night-rain.svg";
     }
     
-
+    getForecast(response.data.name);
 }
 function formatDate(date){
   let minutes = date.getMinutes();
   let hours = date.getHours();
+  let ampm = hours >=12 ? 'PM': 'AM';
+  let formattedHours = hours % 12 || 12; 
+
   let days = [
     "Sunday",
     "Monday",
@@ -54,14 +57,15 @@ function formatDate(date){
     "Saturday" 
      ];
   let day = days[date.getDay()];
+  
+
   if(minutes < 10){
     minutes = `0${minutes}`
    };
-  return`${day} ${hours}:${minutes}`;
+  return`${day} ${formattedHours}:${minutes}`;
   
 
 }
-
 
 function searchCity(city){
     let apiKey = "50da24d4c009f8e56953c014274fe893";
@@ -79,7 +83,80 @@ function handleSearchSubmit(event){
 
 }
 
+function formartDay(timestamp){
+ let date = new Date (timestamp * 1000);
+ let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri","Sat"];
+
+ return days[date.getUTCDay()];
+
+}
+
+function getForecast(city) {
+ let apiKey = "50da24d4c009f8e56953c014274fe893";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(displayForecast);
+
+
+}
+
+function displayForecast(response){
+ 
+    let forecastHtml = "";
+
+    response.data.list.forEach(function(day, index) {
+      if(index < 5) {
+        let weatherIconSrc;
+
+        switch (day.weather[0].main) {
+            case "Clouds":
+                weatherIconSrc = "src/cloudy.svg";
+                break;
+            case "Clear":
+                weatherIconSrc = "src/clear-day.svg";
+                break;
+            case "Rain":
+                weatherIconSrc = "src/rain.svg";
+                break;
+            case "Drizzle":
+                weatherIconSrc = "src/drizzle.svg";
+                break;
+            case "Mist":
+                weatherIconSrc = "src/mist.svg";
+                break;
+            case "Snow":
+                weatherIconSrc = "src/snow.svg";
+                break;
+            case "Thunderstorm":
+                weatherIconSrc = "src/thunderstorms-night-rain.svg";
+                break;
+            default:
+                weatherIconSrc = ""; 
+        }
+            forecastHtml += 
+            `
+            <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formartDay(day.dt)}</div>
+            
+            <img src="${weatherIconSrc}" class="weather-forecast-icon"/>
+            
+            <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature">
+             <strong>${Math.round(day.main.temp_max)}°</strong> 
+             </span>
+             <span class="weather-forecast-temperature">${Math.round(day.main.temp_min)}°</span>
+            </div>
+            </div>
+            `;
+      }
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sydney");
+
+
